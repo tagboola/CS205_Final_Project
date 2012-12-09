55de6e0a3be44332304404d121c6481541a307d1
Tunde <tun.agboola@gmail.com>
2012-12-08 23:06:34 -0500

    peer review

--- data_handler.py
+++ data_handler.py
@@ -12,17 +12,39 @@ class DataHandler:
 	def get_shape():
 		return self.data.shape
 
-	def get_labels(indices):
-		labels = {}
+	def get_freq(indices):
+		freq = {}
 		for index in indices:
-			label = self.data[index][0]
-			if label in labels:
-				labels[label] = 1
+			key = data[index][LABEL_INDEX]
+			if freq.has_key(key):
+				freq[key] += 1
 			else:
-				labels += 1
-		return labels
+				freq[key] = 1
 
-	def purity(feature, threshold, indices):
+	def get_labels(indices):
+		return get_freq(indices)
+
+
+	def purity(indices):
+		freq = get_freq(indices)
+		gini = 0
+		for value in freq.values():
+			gini += math.pow(float(value)/len(indices),2)
+		return 1 - gini
+	
+	def get_threshold(feature, indices):
+		min_val = 10000000
+		max_val = -1
+		for index in indices:
+			val = data[index][feature]
+			if val > max_val:
+				max_val = val
+			if val < min_val:
+				min_val = val
+		return random.random()*(max_val-min_val) + min_val		
+
+	def test_split(feature, threshold, indices):
+		threshold = get_threshold(feature, indices)		
 		# Initialize variables
 		freq1, freq2 = {}, {}
 		n1, n2 = 0, 0
@@ -32,13 +54,13 @@ class DataHandler:
 			key = data[index][0]
 			if data[index][feature] <= threshold:
 				n1 += 1
-				if freq1.has_key():
+				if freq1.has_key(key):
 					freq1[key] += 1
 				else:
 					freq1[key] = 1  
 			else:
 				n2 += 1
-				if freq2.has_key():
+				if freq2.has_key(key):
 					freq2[key] += 1
 				else:
 					freq2[key] = 1
@@ -47,9 +69,9 @@ class DataHandler:
 		print subset2
 		gini1, gini2 = 0, 0
 		#Calculate the Gini impurity of both sets
-		for freq in freq1.val():
+		for freq in freq1.values():
 			gini1 += math.pow(freq/float(n1),2)
-		for freq in freq2.val():
+		for freq in freq2.values():
 			gini2 += math.pow(freq/float(n2),2)
 		gini1 = 1 - gini1
 		gini2 = 1 - gini2
@@ -57,9 +79,7 @@ class DataHandler:
 		# of each subset
 		total_gini = (n1/float(n1+n2))*gini1 + (n2/float(n1+n2))*gini2
 		print total_gini
-		return total_gini
-		  
-		return 
+		return (total_gini, threshold)
 		 
 				
 			
@@ -68,7 +88,7 @@ class DataHandler:
 	def split(indices, feature, threshold):
 		left, right = [], []
 		for index in indices:
-			if self.data[index][feature] >= threshold:
+			if self.data[index][feature] <= threshold:
 				left.append(index)
 			else:
 				right.append(index)
--- subset.py
+++ subset.py
@@ -4,12 +4,10 @@ from subset_base import SubsetBase
 
 class Subset(SubsetBase):
 
-	PURITY_THRESHOLD = 0.95
-	K_FEATURES = 10
+	K_FEATURES = 2
 
-	def __init__(self, indices, features, data_handler=None, data=None):
+	def __init__(self, indices, data_handler=None, data=None):
 		self.indices = indices
-		self.features = features
 		self.labels = None
 		if data_handler:
 			self.data_handler = data_handler
@@ -18,26 +16,20 @@ class Subset(SubsetBase):
 
 	def pure():
 		"""Determine if subset is pure. Returns label of pure item if pure and None otherwise"""
-		if not self.labels:
-			self.labels = self.data_handler.get_labels()
+		return self.data_handler.purity(self.indices)
 
-		if len(self.labels) == 1:
-			return self.labels.keys()[0]
-
-		return None
 
 
 	def majority_label():
 		"""Return label of majority item."""
-		if not self.labels:
-			self.labels = self.data_handler.get_labels()
+		labels = self.data_handler.get_labels(self.indices)
 
 		majority, count = None, -1
-		for label, value in self.labels.iteritems():
+		for label, value in labels.iteritems():
 			if value > count:
 				majority = label
 
-		return label
+		return majority
 
 	def split():
 		"""
@@ -46,30 +38,24 @@ class Subset(SubsetBase):
 		"""
 		n, f = self.data_handler.get_shape()
 		# Selects k features without replacement
-		#TODO match up the features 1 is star rating? if so ignore that feature
-		features = random.sample(range(f), K_FEATURES) 
+		features = random.sample(range(1, f), K_FEATURES) 
 
-		#try different splits
+		# Try k different splits
 		splits = {}
 		for feature in features:
-			threshold = #TODO - come up with the threshold?
-			purity = purity(feature, threshold)			
-			splits[feature] = {'threshold':threshold, 'purity':purity}
+			(gini, threshold) = self.data_handler.test_split(feature)			
+			splits[feature] = {'threshold':threshold, 'gini':gini}
 
-		#finds optimal split, from all tests
-		best_feature, threshold, max_purity = None, None, -1
+		# Finds the optimal split from all test
+		best_feature, threshold, min_gini = None, None, 100
 		for feature, results in splits.iteritems():
-			if results['purity'] > max_purity:
-				best_feature, threshold, max_purity = feature, results['threshold'], results['purity']
+			if results['gini'] < min_gini:
+				best_feature, threshold, min_gini = feature, results['threshold'], results['gini']
 
 		#Split remaining data
 		subset_left, subset_right = get_subsets(best_feature, threshold)
 		return best_feature, threshold, subset_left, subset_right
 
-
-	def purity(feature, threshold):
-		
-		return 
 
 	def get_subsets(feature, threshold):
 		left, right = self.data_handler.split(self.indices, feature, threshold)
@@ -78,9 +64,6 @@ class Subset(SubsetBase):
 		return left_subset, right_subset
 
 
-	def test_purity_on_split(feature, threshold):
-		purity = 0
-		return purity
 
 
 
